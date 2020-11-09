fd01150dbd93989fdd1e343e7e378e9ce0f1b5be
Tony Zhu <t.zhu@f5.com>
2020-11-08 16:28:54 -0800

    failed to build

--- run-node.sh
+++ run-node.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o pipefail
 
 node_number=$1
+join_addr=$2
 
 function usage() {
     echo "Usage:"
@@ -20,6 +21,10 @@ if ! [[ "${node_number}" =~ ^[1-5]$ ]] ; then
     exit 1
 fi
 
+if [ -z ${join_addr} ] ; then
+    join_addr="127.0.0.1:8000"
+fi
+
 node_raft_port=$((6999 + $node_number))
 node_http_port=$((7999 + $node_number))
 data_directory="./node${node_number}"
@@ -31,7 +36,8 @@ args="${args} --http-port=${node_http_port}"
 if [ "${node_number}" == 1 ] ; then
     args="${args} --bootstrap true"
 else
-    args="${args} --join=127.0.0.1:8000"
+    
+    args="${args} --join=$join_addr"
 fi
 
 ./my-raft ${args}
